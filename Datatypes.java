1d25b6ec4f0092edca33ec8221fbd61919f5968a
Giridharmani <162684584+Giridharmani@users.noreply.github.com>
2024-06-26 15:37:26 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,20 @@
+package hometask1;
+
+public class Datatypes {
+	
+	public static void main(String[] args) {
+		float SquareValue = 91.0f; //to store float value append f at the end 
+		String SearchEngine = "Google";// to store sequence of char double quotes required
+		boolean areyouknowlearningjava = true;
+		int year = 1994;
+		char logo = 'C'; // single quotes for character
+		System.out.println("SquareValue is "+SquareValue);
+		System.out.println("SearchEngine is "+SearchEngine);
+		System.out.println("Are you know  java is " +areyouknowlearningjava);
+		System.out.println("Year is "+year );
+		System.out.println("Logo of company is "+logo);
+		
+		
+	}
+
+}
