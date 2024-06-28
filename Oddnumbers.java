f06c7feeb414da3a514c864c29b9e2208e595593
Giridharmani <162684584+Giridharmani@users.noreply.github.com>
2024-06-28 22:00:21 +0530

    Add files via upload

---
+++ Oddnumbers.java
@@ -0,0 +1,19 @@
+package HomeAssignemnt;
+
+public class Evennumbers { 
+	public static void main(String args[])   
+	{  
+	int number=10;  
+	System.out.println("List of odd numbers from 1 to 10");  
+	for (int i=1; i<=number; i++)   
+	{  
+	//logic to check if the number is even or not  
+	//if i%2 is equal to zero, the number is even 
+		//if i%2 is equal to one,the number is odd
+	if (i%2==1)   
+	{  
+	System.out.println(i + " ");  
+	}  
+	}  
+	}  
+	}  
